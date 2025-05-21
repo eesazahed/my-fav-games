afa9d6234e47e32bacc1f279a6d2761937f6ea38
Eesa Zahed <eesa.zahed@gmail.com>
2025-05-21 18:20:31 -0400

    minify js

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 setInterval(() => {
-  const fallingItem = document.createElement("div");
-  fallingItem.textContent = "🎮";
-  fallingItem.className = "fallingItem";
-
-  fallingItem.style.left = `${Math.random() * window.innerWidth}px`;
-  fallingItem.style.fontSize = `${10 + Math.random() * 20}px`;
-  fallingItem.style.animationDuration = `${3 + Math.random() * 5}s`;
-  fallingItem.style.animationDelay = `${Math.random() * 4}s`;
-
-  document.body.appendChild(fallingItem);
-
-  fallingItem.addEventListener("animationend", () => fallingItem.remove());
+  let e = document.createElement("div");
+  (e.textContent = "\uD83C\uDFAE"),
+    (e.className = "fallingItem"),
+    (e.style.left = `${Math.random() * window.innerWidth}px`),
+    (e.style.fontSize = `${10 + 20 * Math.random()}px`),
+    (e.style.animationDuration = `${3 + 5 * Math.random()}s`),
+    (e.style.animationDelay = `${4 * Math.random()}s`),
+    document.body.appendChild(e),
+    e.addEventListener("animationend", () => e.remove());
 }, 200);
